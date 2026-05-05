31da419d853c7b4a4aacbb89a5f1d8e15c4bb87f
sofiamaia-creator <sofia_maia@estudante.sesisenai.org.br>
2026-05-05 10:10:25 -0300

    javascript jogo

---
+++ jogo.js
@@ -0,0 +1,60 @@
+const COLUNAS = 20;
+const LINHAS = 20;
+const PONTOS_POR_COMIDA = 20;
+
+const VELOCIDADE =  parseInt(localStorage.getItem("snake_velocidade")) || 130;
+
+let cobra = [];
+let direcao = { x: 1, y: 0};
+let proximaDirecao = { x: 2, y: 0}; //Dá o passo pra direita
+let comida = { x: 0, y: 0}; //Ponto inicial da comida
+let pontuacao = 0;
+let recorde = parseInt(localStorage.getItem("snake_recorde")) || 0;
+let intervalo = null;
+let emJogo = false;
+
+
+//GRADE
+const grade = document.getElementById("grade");
+let celulas = []; //Aqui vamos armazenar as 400 células
+
+function criarGrade() {
+    grade.innerHTML = "";
+    celulas = [];
+
+
+    for (let y = 0; y < LINHAS; y++) {
+        const linha = [];
+
+        for (let x = 0; x < COLUNAS; x++) {
+            const celula = document.createElement("div");
+            celula.className = "celula";
+            grade.appendChild(celula);
+            linha.push(celula);
+        }
+        celulas.push(linha);
+    }
+}
+
+function limparGrade() {
+
+    for(let y = 0; y < LINHAS; y++) {
+        for(let x = 0; x <LINHAS; x++){
+            celulas[y][x].className = "celula";
+        }
+    }
+}
+
+function renderizar() {
+    limparGrade();
+
+    //Desenhar a comida
+    celulas[comida.y][comida.x].classList.add("comida");
+
+    //Desenhar o corpo da cobra
+    for (let i = 1; i < cobra.length; i++) {
+        celulas[cobra[i].y][cobra[i].x].classList.add("cobra");
+    } //cobra = [cabeça, segmento do corpo, segmento do corpo]
+
+    celulas[cobra[0].y][cobra[0].x].classList.add("cabeca");
+}
